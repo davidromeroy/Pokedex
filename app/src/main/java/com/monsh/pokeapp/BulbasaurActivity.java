da729de0e66d230e6cf6dbda8096eaee7bd252ed
Monserrat Ramirez <Monserrat Ramirez>
2017-05-03 16:08:38 -0500

    App from Workshop: 'How to create your pokedex on Android'

---
+++ app/src/main/java/com/monsh/pokeapp/BulbasaurActivity.java
@@ -0,0 +1,16 @@
+package com.monsh.pokeapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by monsh on 03/05/2017.
+ */
+
+public class BulbasaurActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bulbasaur);
+    }
+}
---
+++ app/src/main/java/com/monsh/pokeapp/pokeapi/PokeapiService.java
@@ -0,0 +1,16 @@
+package com.monsh.pokeapp.pokeapi;
+
+import com.monsh.pokeapp.models.PokemonRespuesta;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by monsh on 02/05/2017.
+ */
+
+public interface PokeapiService {
+    @GET("pokemon")
+    Call<PokemonRespuesta> obtenerListaPokemon(@Query("limit")int limit, @Query("offset")int offset);
+}
